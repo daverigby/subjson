41306ac832899950fa7249f0e9c1f457a29d5c29
Mark Nunberg <mnunberg@haskalah.org>
2015-05-06 11:17:30 -0700

    Fix deletion of final array element
    Change-Id: Iad5c74e7f39d2e7beca55e5a7d2b2bfcbc650452

--- subdoc/match.cc
+++ subdoc/match.cc
@@ -151,13 +151,13 @@ push_callback(jsonsl_t jsn, jsonsl_action_t action, struct jsonsl_state_st *st,
                 m->loc_key.length = ctx->hklen+2;
 
                 // I'm not sure if it's used.
-                m->position = (st->nelem - 1) / 2;
+                m->position = (parent->nelem - 1) / 2;
             } else {
 
                 // Array doesn't have a key
                 m->has_key = 0;
                 // array[n]
-                m->position = st->nelem - 1;
+                m->position = parent->nelem - 1;
             }
 
             if (m->ensure_unique.at) {
@@ -454,7 +454,7 @@ Match::exec_match_negix(const char *value, size_t nvalue, const Path *pth,
             }
 
             /* Set the position */
-            position = num_siblings - 1;
+            position = num_siblings;
 
             last_start = loc_match.at;
             last_len = loc_match.length;
